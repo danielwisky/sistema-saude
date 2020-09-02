b6f70a5e2d6f6f5459f43c61c051d65247376db3
Daniel Dias Wisky <daniel-ddw@hotmail.com>
2020-09-02 15:32:52 -0300

    update branch

--- src/main/java/br/com/sistema/saude/gateways/kafka/Covid19AsyncGatewayImpl.java
+++ src/main/java/br/com/sistema/saude/gateways/kafka/Covid19AsyncGatewayImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class Covid19SyncGatewayImpl implements Covid19AsyncGateway {
+public class Covid19AsyncGatewayImpl implements Covid19AsyncGateway {
 
   private final KafkaTemplate<String, String> kafkaTemplate;
 
